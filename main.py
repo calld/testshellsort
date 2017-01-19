4b4efadc590707bbadaf594e6407010414b9ca75
Devon Call <callbrentdevon@gmail.com>
2017-01-18 16:29:56 -0800

    added timing to main

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import shellsort as ss
 import random as ran
+import timeit
 
 reverselist = [x for x in range(10000, 0, -1)]
 
 randomlist = [ran.randrange(1000000) for x in range(10000)]
 
-print("before: " + " ".join(map(lambda x: str(x), reverselist)) + "\n")
-ss.shellsort(reverselist)
-print("after: " + " ".join(map(lambda x: str(x), reverselist)) + "\n")
+print("before: " + " ".join(map(str, reverselist)) + "\n")
+time = timeit.timeit('ss.shellsort(reverselist)', number = 1, globals = globals())
+print("after: " + " ".join(map(str, reverselist)) + "\n")
+print("completion: " + str(time) + "\n")
 
-print("before: " + " ".join(map(lambda x: str(x), randomlist)) + "\n")
-ss.shellsort(randomlist)
-print("after: " + " ".join(map(lambda x: str(x), randomlist)) + "\n")
+print("before: " + " ".join(map(str, randomlist)) + "\n")
+time = timeit.timeit('ss.shellsort(randomlist)', number = 1, globals = globals())
+print("after: " + " ".join(map(str, randomlist)) + "\n")
+print("completion: " + str(time) + "\n")
