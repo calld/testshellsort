fd1c16e10dee8059258e2267f2cfb06882e773c9
Devon Call <callbrentdevon@gmail.com>
2017-02-07 12:04:35 -0800

    added test of quicksort vs shellsort

---
+++ quickvshell.py
@@ -0,0 +1,26 @@
+import quicksort as qs
+import shellsort as ss
+import random as ran
+import timeit
+
+gaps = [100, 59, 39, 27, 21, 16, 13, 11, 9, 4, 1] #best result from evolution search
+
+
+def ranlist(size = 10000, rang = 10000):
+    return [ran.randrange(rang) for x in range(size)]
+
+n = 1000
+
+testset = [[]] + [ranlist() for x in range(n)]
+
+def testiter(inp):
+    for x in [y[:] for y in inp]:
+        yield x
+
+testsetup = 'ti = testiter(testset); next(ti)'
+
+qtime = timeit.timeit('qs.quicksort(next(ti))', testsetup, number = n, globals = globals())
+stime = timeit.timeit('ss.shellsort(next(ti), gaps = gaps)', testsetup, number = n, globals = globals())
+
+print("quicksort time: {}".format(qtime))
+print("shellsort time: {}".format(stime))
